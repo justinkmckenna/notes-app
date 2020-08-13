325611516e16ab218f8e1672dc93b6b9a0a4ce19
Justin Mckena <mckennaman123@gmail.com>
2020-08-13 09:24:07 -0400

    update app version

--- get.js
+++ get.js
@@ -6,7 +6,7 @@ export const main = handler(async (event, context) => {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
     // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
+    // - 'noteId': path parameter.
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: event.pathParameters.id
